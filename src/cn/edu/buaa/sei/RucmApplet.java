579ce3b204fe07fc703733975d405e45a6613c28
Jinghui Hu <hujinghui@buaa.edu.cn>
2016-05-02 17:48:28 +0800

    添加RUCM读取功能

---
+++ src/cn/edu/buaa/sei/RucmApplet.java
@@ -0,0 +1,36 @@
+package cn.edu.buaa.sei;
+
+
+import cn.edu.buaa.sei.rucm.RucmReader;
+import cn.edu.buaa.sei.rucm.ds.UseCase;
+import cn.edu.buaa.sei.util.ConfigMgr;
+import cn.edu.buaa.sei.util.LoggerMgr;
+import cn.edu.buaa.sei.util.StopWatch;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * Created by hujh on 5/2/16.
+ */
+public class RucmApplet {
+    private static AppConfig conf;
+    private static Logger logger;
+
+    public static void main(String[] args) {
+        conf = ConfigMgr.getConfig();
+        logger = LoggerMgr.getLogger();
+        StopWatch watch = new StopWatch();
+        RucmReader reader = new RucmReader();
+
+        String path = conf.getPath2WordOutput() + "rucm";
+        String[] files = new File(path).list();
+        for (String file : files) {
+            reader.processFile(path + "/" + file);
+        }
+
+        logger.info("AppMain finished, elapsedTimes: " + watch.getElapsedTimeInSeconds() + "s");
+    }
+}
---
+++ src/cn/edu/buaa/sei/rucm/RucmReader.java
@@ -0,0 +1,101 @@
+package cn.edu.buaa.sei.rucm;
+
+import cn.edu.buaa.sei.AppConfig;
+import cn.edu.buaa.sei.rucm.ds.*;
+import cn.edu.buaa.sei.util.ConfigMgr;
+import cn.edu.buaa.sei.util.GenericFileIO;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.StringCodec;
+
+/**
+ * Created by hujh on 5/2/16.
+ */
+public class RucmReader {
+    GenericFileIO gfio = null;
+    AppConfig conf;
+
+    public RucmReader () {
+        conf = ConfigMgr.getConfig();
+        gfio = new GenericFileIO();
+    }
+
+    public UseCase loadFile(String filename) {
+        String text = gfio.read(filename);
+        if (null == text) return null;
+        return JSON.parseObject(text, UseCase.class);
+    }
+
+
+    public void processFile(String filename) {
+        UseCase uc = loadFile(filename);
+        String text = rucm2String(uc);
+        String outFileName = conf.getPath2WordOutput() + filename.substring(filename.indexOf("/")+2) + ".txt";
+        gfio.write(text, outFileName);
+    }
+
+    private String rucm2String(UseCase uc) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        sb.append(String.format("UseCase  : %s\n", uc.getUseCaseName()));
+        i = 0;
+        for (Input input : uc.getInputs()) {
+            if (i==0)
+                sb.append(String.format("Input    : %s\n", input.getText()));
+            else
+                sb.append(String.format("         : %s\n", input.getText()));
+            ++i;
+        }
+        i = 0;
+        for (Output output: uc.getOutputs()) {
+            if (i==0)
+                sb.append(String.format("Output   : %s\n", output.getText()));
+            else
+                sb.append(String.format("         : %s\n", output.getText()));
+            ++i;
+        }
+        i = 0;
+        for (Condition cond : uc.getPreConditions()) {
+            if (i==0)
+                sb.append(String.format("PreCond  : %s\n", cond.getText()));
+            else
+                sb.append(String.format("         : %s\n", cond.getText()));
+            ++i;
+        }
+        i = 0;
+        for (Step step : uc.getBasicFlow()) {
+            if (i==0)
+                sb.append(String.format("BasicFlow: %d.%s\n", ++i, step.getText()));
+            else
+                sb.append(String.format("         : %d.%s\n", ++i, step.getText()));
+        }
+        i = 0;
+        for (Condition cond : uc.getPostConditions()) {
+            if (i==0)
+                sb.append(String.format("PostCond : %s\n", cond.getText()));
+            else
+                sb.append(String.format("         : %s\n", cond.getText()));
+            ++i;
+        }
+        for (Flow flow : uc.getAlterFlow()) {
+            String rfs = "RFS ";
+            for (Integer iRfs : flow.getBasicFlowSteps())
+                rfs += iRfs + ",";
+            sb.append(String.format("AlterFlow: %s\n", rfs));
+            int j = 0;
+            for (Step step : flow.getSteps()) {
+                sb.append(String.format("           %d.%s\n", ++j, step.getText()));
+            }
+            j = 0;
+            for (Condition cond : uc.getPostConditions()) {
+                if (j==0)
+                    sb.append(String.format(" PostCond: %s\n", cond.getText()));
+                else
+                    sb.append(String.format("         : %s\n", cond.getText()));
+                ++j;
+            }
+        }
+
+        return sb.toString();
+    }
+}
